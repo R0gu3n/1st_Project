bdaef171ac7928eb8175926b7e090b742c53122f
Rogu3n <77072694+Rogu3n@users.noreply.github.com>
2021-08-04 11:10:12 -0700

    Created Exchange tab and added extra pages
    I created an exchange tab that shows the top 50 as well as cleaning up a little code. After that I decided to give the page buttons functionality so I created an extra page each for the currencies and the exchanges.

--- logicNext.js
+++ logicNext.js
@@ -1,5 +1,5 @@
 var baseURL = "https://api.coingecko.com/api/v3";
-var coinDataURL = "/coins/markets?vs_currency=usd&order=market_cap_desc%2C%20volume_desc%2C%20id_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h";
+var coinDataURL = "/coins/markets?vs_currency=usd&order=market_cap_desc%2C%20volume_desc%2C%20id_desc&per_page=200&page=1&sparkline=false&price_change_percentage=24h";
 var url1 = baseURL + coinDataURL;
 
 
@@ -7,7 +7,7 @@ var url1 = baseURL + coinDataURL;
 fetch(url1)
 .then(res =>{
   res.json().then(data =>{
-    for(var i=0; i<100; i++){
+    for(var i=100; i<200; i++){
       let rank = data[i].market_cap_rank;
       let pic = data[i].image;
       let coin = data[i].name;
@@ -19,12 +19,12 @@ fetch(url1)
       let positive = "+";
 
       if(change < 0){
-        $("#tableBody1").append("<tr><td>"+ rank + "</td>"+ "<td><img class= 'image' src= "+ pic +"/>"+ coin + "</td>"+
+        $("#tableBody1Next").append("<tr><td>"+ rank + "</td>"+ "<td><img class= 'image' src= "+ pic +"/>"+ coin + "</td>"+
            "<td>"+ mktcap + "</td>"+ "<td>"+ price + "</td>"+ "<td>"+ volume + "</td>"+
            "<td>"+ supply + "</td>"+ "<td class='red'>"+ change +  "%</td></tr>");
       }
       if(change > 0){
-        $("#tableBody1").append("<tr><td>"+ rank + "</td>"+ "<td><img class= 'image' src= "+ pic +"/>"+ " "+ coin + "</td>"+
+        $("#tableBody1Next").append("<tr><td>"+ rank + "</td>"+ "<td><img class= 'image' src= "+ pic +"/>"+ " "+ coin + "</td>"+
            "<td>"+ mktcap + "</td>"+ "<td>"+ price + "</td>"+ "<td>"+ volume + "</td>"+
            "<td>"+ supply + "</td>"+ "<td class='green'>"+ positive + change +  "%</td></tr>");
       };
@@ -33,5 +33,5 @@ fetch(url1)
 })
 
 .catch(err =>{
-  $("#tableBody1").html(err);
+  $("#tableBody1Next").html(err);
 });
